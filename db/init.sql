1415d58b78fc883df0ca3b069dee6298299115eb
Filipe Oliveira <filipao.br@hotmail.com>
2025-10-01 16:07:01 -0300

    Backend com SQLLITE

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+-- Tabela de Produtos
+CREATE TABLE produtos (
+    id INTEGER PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    valor_unitario DECIMAL(10, 2) NOT NULL
+);
+
+-- Tabela de Clientes
+CREATE TABLE clientes (
+    id INTEGER PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+-- Tabela de Vendas
+CREATE TABLE vendas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    produto_id INTEGER REFERENCES produtos(id),
+    cliente_id INTEGER REFERENCES clientes(id),
+    quantidade INTEGER NOT NULL,
+    data_venda DATE NOT NULL
+);
